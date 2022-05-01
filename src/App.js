c275e9aceb38bd318f06972c978f4414ab9812ca
VP <huuwulleop@hotmail.com>
2022-04-30 22:28:39 -0400

    Add sample articles

--- src/App.js
+++ src/App.js
@@ -1,6 +1,25 @@
 
 const title = "HackerNews Reader"
 
+const list = [
+  {
+    title: 'React',
+    url: 'https://reactjs.org/',
+    author: 'Jordan Walke',
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+  },
+  {
+    title: 'Redux',
+    url: 'https://redux.js.org/',
+    author: 'Dan Abramov, Andrew Clark',
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
+  },
+]
+
 const App = () => {
   return (
     <div>
@@ -8,6 +27,12 @@ const App = () => {
 
       <label htmlFor="search">Search: </label>
       <input type="text" id="search" />
+
+      <hr />
+
+      {list.map(item => (
+        <div key={item.objectID}>{item.title}</div>
+      ))}
     </div>
   )
 }
