410d99a7f65fec0bc802aa50f4239105818d9270
VP <huuwulleop@hotmail.com>
2022-04-30 22:43:40 -0400

    Add search term

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
+import React, { useState } from "react"
+
+// components
 import List from "./components/List"
 
 const title = "HackerNews Reader"
 
-const list = [
+const stories = [
     {
         title: 'React',
         url: 'https://reactjs.org/',
@@ -22,16 +25,23 @@ const list = [
 ]
 
 const App = () => {
+    const [searchTerm, setSearchTerm] = useState('')
+    
+    const handleChange = event => {
+        setSearchTerm(event.target.value)
+        console.log(event.target.value);
+    }
+
     return (
         <div>
             <h1>Welcome to {title}</h1>
 
             <label htmlFor="search">Search: </label>
-            <input type="text" id="search" />
+            <input type="text" id="search" onChange={handleChange} />
 
             <hr />
 
-            <List list={list} />
+            <List list={stories} />
         </div>
     )
 }
