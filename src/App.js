c5d54fbdfa798438950e3f08ecd5b4651f09ba57
VP <huuwulleop@hotmail.com>
2022-05-03 00:11:13 -0400

    Add loading

--- src/App.js
+++ src/App.js
@@ -41,11 +41,15 @@ const App = () => {
     const [searchTerm, setSearchTerm] = useSemiPersistentState("search", "React")
 
     const [stories, setStories] = useState([])
+    const [isLoading, setIsLoading] = useState(false)
 
     // Get stories
     useEffect(() => {
+        setIsLoading(true)
+
         getAsyncStories().then(result => {
             setStories(result.data.stories)
+            setIsLoading(false)
         })
     }, [])
 
@@ -74,8 +78,12 @@ const App = () => {
             <Search onSearch={handleSearch} searchTerm={searchTerm} />
 
             <hr />
+            {isLoading ? (
+                <p>Loading articles...</p>
+            ) : (
+                <List list={searchedStories} onRemoveItem={handleRemoveStory} />
+            )}
 
-            <List list={searchedStories} onRemoveItem={handleRemoveStory} />
         </div>
     )
 }
