bbbbf9067b94485f51266ca5c4857dcd84445bf1
VP <huuwulleop@hotmail.com>
2022-05-03 01:26:42 -0400

    Add hacker news API

--- src/App.js
+++ src/App.js
@@ -76,6 +76,8 @@ const storiesReducer = (state, action) => {
     }
 }
 
+const API_ENDPOINT = "https://hn.algolia.com/api/v1/search?query="
+
 const App = () => {
     const [searchTerm, setSearchTerm] = useSemiPersistentState("search", "React")
 
@@ -95,14 +97,22 @@ const App = () => {
         // setIsLoading(true)
         dispatchStories({ type: "STORIES_FETCH_INIT" })
 
-        getAsyncStories()
+        // getAsyncStories()
+        //     .then(result => {
+        //         // setStories(result.data.stories)
+        //         dispatchStories({
+        //             type: "STORIES_FETCH_SUCCESS",
+        //             payload: result.data.stories,
+        //         })
+        //         // setIsLoading(false)
+        //     })
+        fetch(`${API_ENDPOINT}react`)
+            .then(response => response.json())
             .then(result => {
-                // setStories(result.data.stories)
                 dispatchStories({
                     type: "STORIES_FETCH_SUCCESS",
-                    payload: result.data.stories,
+                    payload: result.hits
                 })
-                // setIsLoading(false)
             })
             .catch(() => (
                 dispatchStories({ type: "STORIES_FETCH_FAILURE" })
